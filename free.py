142fa1d69bf69a9b7d67024eade0bd632ce3905e
𝐌𝐀𝐅𝐈𝐘𝐀\٠\l❼⑧❻ ‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎ <younisxyz18@gmail.com>
2025-09-19 08:10:05 +0500

    Create free.py

---
+++ free.py
@@ -0,0 +1,17 @@
+"""
+    @ code by ---( Younis john )---
+    @ Github : https://github.com/YounisXyz
+    @ WhatsApp : https://wa.me/923194999455
+    
+"""
+import os, sys,time, platform
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import WEHSHI
+elif bit == '32bit':
+    print("\033[0m\033[91m Sorry Your Device Is 32bit, This Tool 32bit Not Sported, Only For \033[92m64Bit\033[0m ]");exit() 
+ 
+ 
+ 
+ 
