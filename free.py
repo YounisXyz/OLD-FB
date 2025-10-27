e0b653319ebd66e1ea15a2a2af21a6ed61b88be3
𝐌𝐀𝐅𝐈𝐘𝐀\٠\l❼⑧❻ ‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎ <younisxyz18@gmail.com>
2025-10-27 10:19:12 +0500

    Update free.py

--- free.py
+++ free.py
@@ -5,22 +5,9 @@
     
 """
 import os, sys, platform
- 
-os.system('rm -rf WEHSHI.so')
- 
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf WEHSHI.so')
-except:
-    pass
- 
- 
+os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('WEHSHI.so'):
-        os.system('curl -L https://github.com/YounisXyz/executables/blob/main/WEHSHI.cpython-312.so?raw=true -o WEHSHI.so') 
-        import WEHSHI
-    else:
-        import WEHSHI
+    import WEHSHI
 elif bit == '32bit':
     print("\033[0m\033[91m Sorry Your Device Is 32bit, This Tool 32bit Not Sported, Only For \033[92m64Bit\033[0m ]");exit() 
